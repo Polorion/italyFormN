90389e83ccb04d4dc22785c6e925ef90ed945ecd
Artem <polorion1@gmail.com>
2024-10-10 16:37:37 +0300

    reli

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import {MainPage} from "./Components/MainPage/MainPage.jsx";
 
 function App() {
 
+    setTimeout(() => {
+        console.log(initParams)
+    }, 0)
     return (
         <>
             <MainPage/>
